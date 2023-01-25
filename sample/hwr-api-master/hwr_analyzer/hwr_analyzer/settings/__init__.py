8a4c807cb8a59173f5969ea3924fe9291b6f91b3
chyccs <chyccs@naver.com>
2023-01-26 01:13:34 +0900

    aaaa

--- sample/hwr-api-master/hwr_analyzer/hwr_analyzer/settings/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base import *
-from .logging import *
-from .document import *
-from .sentry import *
-from .rollbar import *
-from services.restful.documents.authentication import AuthenticationScheme  # skipcq: PY-W2000
--- sample/hwr-api-master/hwr_analyzer/services/websocket/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WebsocketConfig(AppConfig):
-    name = 'services.websocket'
