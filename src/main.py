5e7887d9a508851ba7a7a5aae4cff1510a8eefd2
chyccs <chyccs@naver.com>
2023-01-25 19:46:06 +0900

    aaaa

--- src/main.py
+++ src/main.py
@@ -1,28 +1,35 @@
 import glob
-from os import environ, walk, path
+import keyword
 import os
+from os import (
+    environ,
+    path,
+    walk,
+)
+
 import yake
 from yake.highlight import TextHighlighter
-import keyword
 
-from services import get_github_repo, get_pull_request, update_pull_request
+from services import fetch_pull_request
 
 
 def main():
     owner = os.environ['owner']
-    repository_name = os.environ['repository_name']
+    repository = os.environ['repository']
     pull_request_number = os.environ['pull_request_number']
     access_token = os.environ['access_token']
-    pull_request = get_pull_request(
-        repo=get_github_repo(access_token=access_token, user=owner, repository_name=repository_name), 
+    pull_request = fetch_pull_request(
+        access_token=access_token,
+        owner=owner,
+        repository=repository,
         number=int(pull_request_number),
     )
-    
+
     src_path = environ.get("src_path", default='sample/')
     stopwords = environ.get("stopwords", default=[])
-    
+
     texts = {}
-    
+
     for x in walk(src_path):
         for y in glob.glob(path.join(x[0], '*.py')):
             if not y.startswith('./.venv'):
@@ -36,29 +43,29 @@ def main():
     stopwords.extend(keyword.softkwlist)
 
     kw_extractor = yake.KeywordExtractor(
-        lan="en", 
-        n=3, 
-        top=300, 
-        dedupLim=0.9, 
+        lan="en",
+        n=3,
+        top=300,
+        dedupLim=0.9,
         stopwords=stopwords,
     )
     keywords = kw_extractor.extract_keywords(text)
     keywords = sorted(keywords, key=lambda x: x[1], reverse=True)
-    
+
     for kw, v in keywords:
-        print("yake: ",kw, "/ score", v)
-        
+        print("yake: ", kw, "/ score", v)
+
     th = TextHighlighter(
-        max_ngram_size = 3, 
-        highlight_pre = "`", 
-        highlight_post= "`",
+        max_ngram_size=3,
+        highlight_pre="`",
+        highlight_post="`",
     )
 
     decorated_title = th.highlight(pull_request.title, keywords)
     decorated_body = th.highlight(pull_request.body, keywords)
 
-    update_pull_request(pull_request=pull_request, title=decorated_title, body=decorated_body)
+    pull_request.update(title=decorated_title, body=decorated_body)
 
 
 if __name__ == "__main__":
-	main()
+    main()
--- src/services.py
+++ src/services.py
@@ -1,14 +1,17 @@
-from github import Github
-from github import Repository
+from github import (
+    Github,
+    Repository,
+)
 from github.PullRequest import PullRequest
 
 
-def get_github_repo(access_token, user, repository_name) -> Repository:
-    g = Github(access_token)
-    return g.get_user(user).get_repo(repository_name)
+def fetch_github_repo(access_token, owner, repository) -> Repository:
+    return Github(access_token).get_user(owner).get_repo(repository)
 
-def get_pull_request(repo: Repository, number) -> PullRequest:
-    return repo.get_pull(number=number)
+
+def fetch_pull_request(access_token, owner, repository, number) -> PullRequest:
+    return fetch_github_repo(access_token, owner, repository).get_pull(number=number)
+
 
 def update_pull_request(pull_request: PullRequest, title, body):
     pull_request.edit(title=title, body=body)
